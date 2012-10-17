b5705e4734399418049c2b6020381c994bc1e245
euforic <cs@euforic.co>
2012-10-17 15:14:39 -0700

    fixed textRunner parsing issue

--- Resources/tests/index.js
+++ Resources/tests/index.js
@@ -7,7 +7,7 @@ var BASE_DIR = 'tests/';
 module.exports = function(tests){
 
   tests.forEach(function(test){
-    var testDir = test.replace(/':'/g, '/');
+    var testDir = test.replace(/:/g, '/');
     require(BASE_DIR+testDir);
   });
 
